9125622391eef7dc010b1c43051414844f2b86eb
Jose <jcopoviking@outlook.com>
2016-08-18 12:15:13 +0100

    Ch5 - sets up innerHTML functionality (basic)

--- note_controller.js
+++ note_controller.js
@@ -1,8 +1,12 @@
 (function(exports) {
-  function changeText() {
-    var appDiv = document.getElementById("app")
-    appDiv.innerHTML = "Howdy!";
-  }
 
+  function changeText() {
+    window.onload = function() {
+    var appDiv = document.getElementById("app");
+    appDiv.innerHTML = "Howdy!";
+  };
+}
   exports.changeText = changeText;
 })(this);
+
+changeText();
